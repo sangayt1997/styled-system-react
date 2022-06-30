0c8214ac34c5dfbb73db543b5e9e7893a9a3b493
Sangay Thinley <thinley.sangay@selise.ch>
2022-07-01 00:59:01 +0600

    refactor: using compose styled-system

--- src/shared/box/box.tsx
+++ src/shared/box/box.tsx
@@ -1,13 +1,21 @@
 import styled from 'styled-components'
-import { space, color, layout, ColorProps, SpaceProps, LayoutProps, typography, TypographyProps } from 'styled-system'
+import {
+    space,
+    color,
+    layout,
+    ColorProps,
+    SpaceProps,
+    LayoutProps,
+    typography,
+    TypographyProps,
+    compose
+} from 'styled-system'
 
 type SsBoxProps = LayoutProps & ColorProps & TypographyProps & SpaceProps;
 
-const SsBox = styled.div<SsBoxProps>(
-    typography,
-    space,
-    color,
-    layout
-)
+const SsBox = styled.div<SsBoxProps>`
+  box-sizing: border-box;
+  ${compose(layout, color, typography, space)}
+`;
 
 export default SsBox;
--- src/shared/text/text.tsx
+++ src/shared/text/text.tsx
@@ -1,22 +1,19 @@
 import styled from "styled-components";
 import {
-  typography,
-  color,
-  TypographyProps,
-  LayoutProps,
-  ColorProps,
-  SpaceProps,
-  layout,
-  space
+    typography,
+    color,
+    TypographyProps,
+    LayoutProps,
+    ColorProps,
+    SpaceProps,
+    layout,
+    space, compose
 } from "styled-system";
 
 type SsTextProps = LayoutProps & ColorProps & TypographyProps & SpaceProps;
 
 const SsText = styled.p<SsTextProps>`
-  ${typography};
-  ${layout};
-  ${space};
-  ${color}
+  ${compose(typography, layout, space, color)}
 `;
 
 export default SsText;
